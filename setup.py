bd9bceaca24f33450d08f46982eed7c478eb0904
Justin Nesselrotte <admin@jnesselr.org>
2019-04-30 23:44:24 -0600

    Updated version to 0.3.1 given issues with subscription logic

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(file_name):
 
 setup(
     name="scclient",
-    version="0.3.0",
+    version="0.3.1",
     author="Justin Nesselrotte",
     author_email="admin@jnesselr.org",
     description="A SocketCluster compatible WebSocket library",
